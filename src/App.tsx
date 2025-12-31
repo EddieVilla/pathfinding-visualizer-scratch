d17f97ad5461090ad4654a951967b93560b0abca
Eddie Villa <eddie20villa@gmail.com>
2025-12-30 22:32:17 -0600

    fyi 18:12 in the video

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <PathfindingProvider>
       <h1 class="text-3xl font-bold underline h-screen w-screen bg-blue-500">
-        Hello world!
+        Hello all!
       </h1>
     </PathfindingProvider>
   );
