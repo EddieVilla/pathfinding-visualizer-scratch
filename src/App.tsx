11323247b541641bcce33c530d8318d32c51ae78
Eddie Villa <eddie20villa@gmail.com>
2025-12-31 12:57:13 -0600

    finished SpeedContext 24:38

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ function App() {
   return (
     <PathfindingProvider>
       <TileProvider>
-        <h1 class="text-3xl font-bold underline h-screen w-screen bg-blue-500">
-          Hello all!
-        </h1>
+        <SpeedProvider>
+          <h1 class="text-3xl font-bold underline h-screen w-screen bg-blue-500">
+            Hello all!
+          </h1>
+        </SpeedProvider>
       </TileProvider>
     </PathfindingProvider>
   );
