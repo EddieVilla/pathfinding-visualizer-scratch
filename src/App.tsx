550c379cba3aa904c84dcc8b53eeebcfecf67ac3
Eddie Villa <eddie20villa@gmail.com>
2025-12-30 23:08:32 -0600

    finished TileContext 21:30

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ function App() {
 
   return (
     <PathfindingProvider>
-      <h1 class="text-3xl font-bold underline h-screen w-screen bg-blue-500">
-        Hello all!
-      </h1>
+      <TileProvider>
+        <h1 class="text-3xl font-bold underline h-screen w-screen bg-blue-500">
+          Hello all!
+        </h1>
+      </TileProvider>
     </PathfindingProvider>
   );
 }
---
+++ src/hooks/useTile.tsx
@@ -0,0 +1,11 @@
+
+
+export const useTile = () => {
+    const context = useContext(TileContext)
+
+    if (!context) {
+        throw new Error("useTile must be used within a TileProvider");
+    }
+
+    return context;
+}
