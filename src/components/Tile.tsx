d4a59b9bf77db8c8d4a5d3b36b36a02b12a1ed11
Eddie Villa <eddie20villa@gmail.com>
2026-01-05 17:39:28 -0600

    34:24

---
+++ src/components/Tile.tsx
@@ -0,0 +1,23 @@
+import { twMerge } from "tailwind-merge";
+
+export function Tile({
+  row,
+  col,
+  isStart,
+  isEnd,
+  isTraversed,
+  isWall,
+  isPath,
+}: {
+  row: number;
+  col: number;
+  isStart: boolean;
+  isEnd: boolean;
+  isTraversed: boolean;
+  isWall: boolean;
+  isPath: boolean;
+}) {
+  return (
+    <div />
+  );
+}
